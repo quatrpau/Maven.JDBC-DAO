c8968f9bd7d2f12c2f40cb1d87210cd1bcdb5ee2
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-06 16:28:32 -0500

    Finished add all

--- src/main/java/daos/VeggieDAO.java
+++ src/main/java/daos/VeggieDAO.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VeggieDAO implements DAOInterface<VeggieDTO> {
@@ -16,8 +17,11 @@ public class VeggieDAO implements DAOInterface<VeggieDTO> {
             PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM veggies WHERE id = ?");
             pstmt.setInt(1,id);
             ResultSet rs = pstmt.executeQuery();
+            rs.beforeFirst();
             while(rs.next()){
-
+                if(rs.getInt("id") == id){
+                    return extract(rs);
+                }
             }
         }
         catch(SQLException sqlx){
@@ -25,8 +29,28 @@ public class VeggieDAO implements DAOInterface<VeggieDTO> {
         }
         return null;
     }
+    public VeggieDTO extract(ResultSet rs) throws SQLException{
+        return new VeggieDTO(rs.getInt("id")
+                ,rs.getString("name")
+                ,Type.valueOf(rs.getString("type"))
+                ,Status.valueOf(rs.getString("status"))
+                ,rs.getString("last_name"));
+    }
 
     public List<VeggieDTO> findAll() {
+        Connection connection = GetConnected.getConnected();
+        try {
+            List<VeggieDTO> veggies = new ArrayList<VeggieDTO>();
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM veggies");
+            ResultSet rs = pstmt.executeQuery();
+            rs.beforeFirst();
+            while(rs.next()){
+                veggies.add(extract(rs));
+            }
+        }
+        catch(SQLException sqlx){
+            sqlx.printStackTrace();
+        }
         return null;
     }
 
@@ -40,5 +64,5 @@ public class VeggieDAO implements DAOInterface<VeggieDTO> {
 
     public void delete(int id) {
     }
-    
+
 }
