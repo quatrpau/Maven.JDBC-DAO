7bc6d17d5da2126f8ebbaef065fdf0f8a561f428
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-06 16:05:15 -0500

    Attempting to implement the getById method

--- src/main/java/daos/VeggieDAO.java
+++ src/main/java/daos/VeggieDAO.java
@@ -1,26 +1,44 @@
 package daos;
 
 import models.DAOInterface;
+import models.GetConnected;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-public class VeggieDAO implements DAOInterface<E> {
-    public E findById(int id) {
+public class VeggieDAO implements DAOInterface<VeggieDTO> {
+    public VeggieDTO findById(int id) {
+        Connection connection = GetConnected.getConnected();
+        try {
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM veggies WHERE id = ?");
+            pstmt.setInt(1,id);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+
+            }
+        }
+        catch(SQLException sqlx){
+            sqlx.printStackTrace();
+        }
         return null;
     }
 
-    public List findAll() {
+    public List<VeggieDTO> findAll() {
         return null;
     }
 
-    public E update(E dto) {
+    public VeggieDTO update(VeggieDTO dto) {
         return null;
     }
 
-    public E create(E dto) {
+    public VeggieDTO create(VeggieDTO dto) {
         return null;
     }
 
     public void delete(int id) {
     }
+    
 }
